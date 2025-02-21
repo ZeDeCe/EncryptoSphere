7542b1c8ef035c72cfa0ad6f68e37990bf1e2483
shaqedmov <shaqedmov@gmail.com>
2025-02-21 19:07:58 +0200

    ADD: Created the Cloud service file that will be used for the abstraction of cloud classes

---
+++ services/CloudService.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+
+class CloudService(ABC):
+    @abstractmethod
+    def authenticate_cloud(self):
+        #Authenticate with the cloud service
+        pass
+
+    @abstractmethod
+    def list_files(self):
+        #List files in the cloud storage, optionally in a specific folder
+        pass
+
+    @abstractmethod
+    def upload_file(self, file_path):
+        #Upload a file to the cloud storage
+        pass
+
+    @abstractmethod
+    def download_file(self, file_name):
+        #Download a file from the cloud storage
+        pass
+
+    @abstractmethod
+    def delete_file(self, file_name):
+        #Download a file from the cloud storage
+        pass
+    @abstractmethod
+    def create_folder(self):
+        #Create a folder in the cloud storage
+        pass
+
+    @abstractmethod
+    def share(self, folder, email):
+        #Share a file or folder with a user (read/write permissions)
+        pass
+ 
+    @abstractmethod
+    def unshare(self):
+        #Unshare a file or folder from a user
+        pass
+
+    @abstractmethod
+    def list_shared_files(self):
+        #List all shared files and folders in the cloud storage
+        pass
