f7c9aedf21d9bb97d0e64df8830b00f132c3e83b
ZeDeCe <no9no3@gmail.com>
2025-03-18 00:46:23 +0200

    add: nosplit

---
+++ modules/Split/NoSplit.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+import Split
+
+class NoSplit(Split):
+    def split_file(self, file, num_parts) -> str:
+        for i in range(0,num_parts-2):
+            shutil.copy(file, file + str(i))
+
+    def merge_parts(self, folder) -> str:
+        return os.path.join(folder, os.listdir(folder)[0])
+    
+    def get_name(self):
+        return "No"
