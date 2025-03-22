f6b0ddf60dec27e6640823634fd2f20ed6311c44
ZeDeCe <no9no3@gmail.com>
2025-03-22 22:05:22 +0200

    add: encrypt can generate and change keys

--- modules/Encrypt/Encrypt.py
+++ modules/Encrypt/Encrypt.py
@@ -1,12 +1,26 @@
 from abc import ABC, abstractmethod
 
 class Encrypt(ABC):
+
+    def __init__(self, key=None):
+        if not key:
+            self.key = self.generate_key()
+        else:
+            self.key = key
+
+    def set_key(self, key) -> None:
+        self.key = key
+
     @abstractmethod
-    def encrypt_file(self, data : bytes):
+    def generate_key() -> any:
+        pass
+
+    @abstractmethod
+    def encrypt_file(self, data : bytes) -> bytes:
         pass
     
     @abstractmethod
-    def decrypt_file(self, data : bytes):
+    def decrypt_file(self, data : bytes) -> bytes:
         pass
 
     @abstractmethod
